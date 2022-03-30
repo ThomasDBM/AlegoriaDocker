7ac8b091662877fab2ee141856a466af24793947
amaryllis <amaryllisvignaud@outlook.fr>
2022-03-30 15:45:16 +0200

    feat(script_implement): change return type and optimize function

--- BDD/script_implement/remove_data.sql
+++ BDD/script_implement/remove_data.sql
@@ -1,36 +1,35 @@
 CREATE EXTENSION IF NOT EXISTS plpython3u;
 
+DROP FUNCTION remove_data(char, integer);
 CREATE OR REPLACE FUNCTION test(tablename char, id int)
-  RETURNS bool
+  RETURNS char
 AS $$
 	if tablename == 'georefs':
 		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return True
+		return 'The georefs table has been removed'
 	if tablename == 'points_appuis':
 		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return True
+		return 'The points_appuis table has been removed'
 	if tablename == 'images':
 		plpy.execute('DELETE FROM points_appuis WHERE id_'+ tablename + ' = ' + str(id))
 		plpy.execute('DELETE FROM georefs WHERE id_'+ tablename + ' = ' + str(id))
 		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return True
+		return 'The images table and its dependencies (georefs, points_appuis) have been deleted'
 	if tablename == 'interne':
 		plpy.execute('DELETE FROM georefs WHERE id_'+ tablename + ' = ' + str(id))
 		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return True
+		return 'The interne table and its dependencies (georefs) have been deleted'
 	if tablename == 'externe':
 		plpy.execute('DELETE FROM georefs WHERE id_'+ tablename + ' = ' + str(id))
 		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return True
+		return 'The externe table and its dependencies (georefs) have been deleted'
 	if tablename == 'transfo2d':
 		plpy.execute('DELETE FROM georefs WHERE id_'+ tablename + ' = ' + str(id))
 		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return True
+		return 'The georefs table and its dependencies (georefs) have been deleted'
 	if tablename == 'masks':
-		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return True
+		return 'To be reviewed'
 	if tablename == 'sources':
-		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return True
+		return 'The source table has a dedicated batch delete function'
 	return False
 $$ LANGUAGE plpython3u;
