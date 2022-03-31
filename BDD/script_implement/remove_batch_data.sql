6f0bb7f0a3017295617e2127070814605113e56d
amaryllis <amaryllisvignaud@outlook.fr>
2022-03-31 15:34:49 +0200

    feat(remove_batch_data.sql) : complete remove_batch_data function

--- BDD/script_implement/remove_batch_data.sql
+++ BDD/script_implement/remove_batch_data.sql
@@ -6,16 +6,20 @@ AS $$
 	if tablename == 'sources':
 	
 		id_images = plpy.execute('SELECT id_images FROM images WHERE id_'+ tablename + ' = ' + str(id))
-		count_id = plpy.execute('SELECT COUNT(id_images) FROM images WHERE id_'+ tablename + ' = ' + str(id))
+		count_id = plpy.execute('SELECT COUNT(id_images) AS tot FROM images WHERE id_'+ tablename + ' = ' + str(id))
 		
 		plpy.notice(id_images.__str__())
-		
-		for i in range(0, count_id, 1):
-			plpy.execute('DELETE FROM points_appuis WHERE id_images = ' + id_images[i]['id_images'])
-			plpy.execute('DELETE FROM georefs WHERE id_images = ' + id_images[i]['id_images'])
-			plpy.execute('DELETE FROM images WHERE id_images = ' + id_images[i]['id_images'])
+		plpy.notice(count_id.__str__())
+
+		for i in range(0, count_id[0]['tot'], 1):
+			plpy.execute('DELETE FROM points_appuis WHERE id_images = ' + str(id_images[i]['id_images']))
+			plpy.execute('DELETE FROM georefs WHERE id_images = ' + str(id_images[i]['id_images']))
+			plpy.execute('DELETE FROM images WHERE id_images = ' + str(id_images[i]['id_images']))
 		
 		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
-		return 'The sources table has a dedicated batch delete function'
-	return 'The function only work with the sources table'
+		return 'The source table has been deleted, along with the associated data set'
+		
+	if tablename == ('masks' or 'transfo2d' or 'externe' or 'interne' or 'points_appuis' or 'images' or 'georefs'):
+		return 'The function only work with the sources table'
+	return 'You must use the sources table with this function'
 $$ LANGUAGE plpython3u;
