5bd4f0ced525aea47781cc944dd8bcd154103ce7
amaryllis <amaryllisvignaud@outlook.fr>
2022-03-31 14:53:25 +0200

    feat(remove_batch_data.sql) : first version of remove_batch_dat function and tests

--- BDD/script_implement/remove_batch_data.sql
+++ BDD/script_implement/remove_batch_data.sql
@@ -1,9 +1,21 @@
 CREATE EXTENSION IF NOT EXISTS plpython3u;
 
-CREATE OR REPLACE FUNCTION remove__batch_data(tablename char, id int)
+CREATE OR REPLACE FUNCTION remove_batch_data(tablename char, id int)
   RETURNS char
 AS $$
 	if tablename == 'sources':
+	
+		id_images = plpy.execute('SELECT id_images FROM images WHERE id_'+ tablename + ' = ' + str(id))
+		count_id = plpy.execute('SELECT COUNT(id_images) FROM images WHERE id_'+ tablename + ' = ' + str(id))
+		
+		plpy.notice(id_images.__str__())
+		
+		for i in range(0, count_id, 1):
+			plpy.execute('DELETE FROM points_appuis WHERE id_images = ' + id_images[i]['id_images'])
+			plpy.execute('DELETE FROM georefs WHERE id_images = ' + id_images[i]['id_images'])
+			plpy.execute('DELETE FROM images WHERE id_images = ' + id_images[i]['id_images'])
+		
+		plpy.execute('DELETE FROM ' + tablename + ' WHERE id_'+ tablename + ' = ' + str(id))
 		return 'The sources table has a dedicated batch delete function'
 	return 'The function only work with the sources table'
 $$ LANGUAGE plpython3u;
