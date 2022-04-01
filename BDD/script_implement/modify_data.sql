db445141d9a51d20c97f54bb4ecfbb08589d87d9
amaryllis <amaryllisvignaud@outlook.fr>
2022-04-01 13:50:52 +0200

    feat(modify_data.sql): modify function because of missing verification by postgre

--- BDD/script_implement/modify_data.sql
+++ BDD/script_implement/modify_data.sql
@@ -22,7 +22,19 @@ CREATE OR REPLACE FUNCTION modify_georefs(id_georefs int DEFAULT -1, user_georef
 										 id_interne int DEFAULT -1, id_externe int DEFAULT -1, id_images int DEFAULT -1)
   RETURNS char
 AS $$
-	if id_georefs > -1:
+
+	id_georefs_tot = plpy.execute('SELECT id_georefs FROM georefs')
+	count_id = plpy.execute('SELECT COUNT(id_georefs) AS tot FROM georefs')
+	
+	plpy.notice(id_georefs_tot.__str__())
+	plpy.notice(count_id.__str__())
+	
+	tab = []
+	for i in range(0, count_id[0]['tot'], 1):
+		tab.append(str(id_georefs_tot[i]['id_georefs']))
+	
+	if id_georefs > -1 and str(id_georefs) in tab:
+		return 'pouet'
 		if user_georef != '':
 			plpy.execute('UPDATE georefs SET user_georef = ' + user_georef)
 		if date != '':
@@ -31,6 +43,10 @@ AS $$
 			plpy.execute('UPDATE georefs SET georef_principal = ' + str(georef_principal))
 		if footprint != '' and epsg != 0:
 			plpy.execute('UPDATE georefs SET footprint = ST_GeomFromText(' + footprint + ', ' + str(epsg) + ')')
+		if near != '' and epsg != 0:
+			plpy.execute('UPDATE georefs SET near = ST_GeomFromText(' + footprint + ', ' + str(epsg) + ')')	
+		if far != '' and epsg != 0:
+			plpy.execute('UPDATE georefs SET far = ST_GeomFromText(' + footprint + ', ' + str(epsg) + ')')
 			
 		return 'All changes done'
 	return 'Nothing to change or wrong id'
--- BDD/script_implement/test/test_modify_data.sql
+++ BDD/script_implement/test/test_modify_data.sql
@@ -17,7 +17,7 @@ SELECT modify_points_appuis(1, '''POINT(2 2)''', '''POINTZ(2 2 2)''', 2154);
 
 --INSERT INTO georefs(id_georefs, user_georef, date, georef_principal, footprint, near, far, id_transfo2d, id_interne, id_externe, id_images) VALUES (4, 'ama4', '2016-06-22 19:10:25-07', TRUE, ST_GeomFromText('POLYGON((50.6373 3.0750,50.6374 3.0750,50.6374 3.0749,50.63 3.07491,50.6373 3.0750))', 2154), ST_GeomFromText('POLYGON((50.6373 3.0750,50.6374 3.0750,50.6374 3.0749,50.63 3.07491,50.6373 3.0750))', 2154), ST_GeomFromText('POLYGON((50.6373 3.0750,50.6374 3.0750,50.6374 3.0749,50.63 3.07491,50.6373 3.0750))', 2154), 4, 4, 4, 4);
 
-SELECT modify_georefs(id_georefs => 1, user_georef => '''AMAMAA''', georef_principal => False);
+SELECT modify_georefs(id_georefs => 1, user_georef => '''AMAMAA''', georef_principal => True);
 
-SELECT modify_georefs(id_georefs => 4, user_georef => '''AMAAMA''', footprint => '''POLYGON((50.6373 3.0750,50.6374 3.0750,50.6374 3.0749,50.63 3.07491,50.6373 3.0750))''',
+SELECT modify_georefs(id_georefs => 4, user_georef => '''AMAAMA''', footprint => '''POLYGON((0 0,0 0,0 0,0 0,0 0))''',
 					 epsg => 2154);
